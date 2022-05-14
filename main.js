3f5a031fb8d6890a899e527efb6b8875bfcb2b95
Justin <jfinney416@gmail.com>
2022-05-14 18:28:31 -0400

    added scramble game

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const { Telegraf, Context } = require("telegraf")
 const bot = new Telegraf(process.env.TELEGRAM_TEST_GOAT_BOT_API_KEY)
 
+const words = ["blue", "red"]
+
+String.prototype.shuffle = function () {
+    let a = this.split(""),
+        n = a.length;
+
+    for (var i = n - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
 // Start Command
 bot.start((context) => context.reply("Welcome little bitch. If you need assistance use the /help command. For all other inquiries or if you want to give suggestions for new features; join @goatbotsupport"))
 
@@ -10,9 +25,25 @@ This is an interactive bot to add to your dumb ass group chats. Currently, you c
 
 /speed - A game used to determine who can write a word correctly and the fastest.
 /taboo - A game that is like charades
-/Scramble = A game centered around players correctly unscambling a scrambled words correct form.
+/scramble = A game centered around players correctly unscambling a scrambled words correct form.
 /leaderboard - A Command used to show leaderboard
 `))
+
+bot.hears("/scramble", context => {
+    let a = words[Math.floor(Math.random() * words.length)]
+    let b = a.shuffle();
+    context.reply("What is " + b + " unscrambled?")
+    while (context.chat != a && context.chat != "exit") {
+        context.reply("That answer was incorrect " + context.from.username + ".")
+    }
+    if (context.chat == "exit") {
+        context.reply("Thanks for playing, you have not exited the game.")
+    }
+    else
+        context.reply(context.from.username + " you are the winner, the answer was " + a + ". To play again type /scramble again.")
+
+})
+
 
 bot.launch();
 
